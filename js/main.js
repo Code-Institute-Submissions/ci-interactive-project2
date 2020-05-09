2c9ebe1199af7d53cb9a546c8a9ec127011bfebc
Oracle <47670366+Oraclebun@users.noreply.github.com>
2020-05-09 09:41:49 +0800

    add dc.js charts, row and pie chart with crossfilter

--- js/main.js
+++ js/main.js
@@ -1,9 +1,19 @@
+const expRowChart = new dc.RowChart("#row-chart");
+const expPieChart = new dc.PieChart("#pie-chart");
+
 $(function(){
 
     $('.sidebarToggle').on('click', function (e) {
         e.preventDefault();
         $('.sidebar').toggleClass('toggled');
     });
+
+        //variables for table
+    let tableData;
+    let tableHeader=[];
+
+    //variables for row/pie chart
+    let expData;    
 
     //API settings
     var settings1 = {
@@ -30,25 +40,63 @@ $(function(){
         }
       }
 
-    //variables for table
-    let tableData;
-    let tableHeader=[];
-
-    //variables for row/pie chart
-    let expData;
-
-
+      //disallow e,+ and - in input
+      $("input[type=number]").keypress(function(evt){
+        if(evt.charCode == 45 || evt.charCode == 43 || evt.charCode == 101) {
+          alert("Keys are not accepted");
+          evt.preventDefault();
+       }
+    });
+    
 
     if (storageAvailable('localStorage')) {
         // localStorage is availables
         let doFirst = getData('localStorage','tableData',settings1);
         let doSecond = getData('localStorage','expData',settings2);
+
         
         $.when(doFirst,doSecond).done(function ( data1, data2 ) {
             tableData=data1;    // "5 objs"
-            expData=data2;      // "54 objs"
-        });
-        
+            expData=data2;      // "54 objs" 
+
+
+            let ndx = crossfilter(expData),
+                quintDim = ndx.dimension(function(d){return d.quintile;}),
+                filteredQuint = quintDim.filter(quintile),
+                byCat = filteredQuint.top(Infinity);
+
+                let tempndx = crossfilter(byCat),
+                    catDim = tempndx.dimension(function(d){return d.category;}),
+                    sourceDim = tempndx.dimension(function(d){return d.source;}),
+                    spendPerCat = catDim.group().reduceSum(function(d){return +d.spending}),
+                    spendPerSource = sourceDim.group().reduceSum(function(d){return +d.spending});
+
+            //    console.log(catDim.top(Infinity));
+
+            pushToTable(tableData);
+            drawBar(tableData);
+            renderPlots(expRowChart, expPieChart, tempndx, catDim, sourceDim, spendPerCat, spendPerSource);
+
+            $("#submit-1").click(function(e){
+                quintile = $("select[name=userQuintile").val();
+                filteredQuint = quintDim.filter(null);
+                populateVar(expData);
+                ndx.remove();
+                ndx.add(expData);
+                filteredQuint = quintDim.filter(quintile);
+                byCat = filteredQuint.top(Infinity);
+                //console.log(byCat);
+                setTimeout(function(){
+                    
+                    tempndx.remove();
+                    tempndx.add(byCat);
+                    dc.redrawAll();
+                }, 1000);
+                $('#formModal').modal('hide');
+                }); 
+            
+
+        }); //end ajax
         } else {
         // Try to use session storage
         console.log("LocalStorage Unavailable");
@@ -62,138 +110,11 @@ $(function(){
  
         }
 
-    
-   pushToTable(tableData);
-
-   let barMargin = {top: 8, right: 30, bottom: 30, left: 50};
-   let barWidth = $("#bar-chart").width()-barMargin.right-barMargin.left;
-   let barHeight = 250-barMargin.top-barMargin.bottom;         /*$("#bar-chart").height();*/
-
-   // List of subgroups = header of the table 
-    let subgroups=Object.keys(tableData[0]).splice(2,3);
-
-    //List of groups
-    let groups = d3.map(tableData, function(d){
-        return(d.quintile)}).keys()
-
-    
-    let listOfValues = tableData.map(d=>d['average total income']).concat(tableData.map(a=>a['average total expenditure']));
-
-    // append the svg object to the body of the page
-    let barSvg = d3.select("#bar-chart")
-                    .append("svg")
-                    .attr("width", barWidth+barMargin.right+barMargin.left)
-                    .attr("height", barHeight+barMargin.top+barMargin.bottom)
-                    .append("g")
-                    .attr("transform","translate(" + barMargin.left + "," + barMargin.top+ ")");
-    
-    let xScale = d3.scaleBand()
-            .domain(groups)
-            .range([0,barWidth])
-            .padding([0.25]);
-    barSvg.append("g")
-            .attr("transform","translate(0," + barHeight + ")")
-            .style("font-size", "0.75rem")
-            .call(d3.axisBottom(xScale).tickSize(0));
-    barSvg.append('text')        
-          .text('Income Quintile')
-          .attr("x", 250)
-          .attr("y", 240);
-        
-    let yScale = d3.scaleLinear()
-            .domain([0, d3.max(listOfValues)])
-            .range([ barHeight, 0 ]).nice();
-
-    let yAxis = d3.axisLeft(yScale)
-                  .tickFormat(function(d,i){
-                      return Math.round(d/1000).toFixed(0);
-                  });
-
-        barSvg.append("g")
-            .call(yAxis);
-            // text label for the y axis
-        barSvg.append("text")
-              .text("$('000)")
-              .attr("x", -150)
-              .attr("y", -30)
-              .attr("transform", "rotate(270)");
-      
-    //subgroup axis
-    let x1 = d3.scaleBand()
-                .domain(subgroups)
-                .range([0,xScale.bandwidth()])
-                .padding([0.05]);
-
-    let chartColor = d3.scaleOrdinal()
-                        .domain(subgroups)
-                        .range(['#68ab77','#d8485c']);
-
-    
-    //Plot the bars
-    barSvg.append("g")
-          .selectAll("g")
-    // Enter in data = loop group per group
-          .data(tableData)
-          .enter()
-          .append("g")
-          .attr("transform", function(d) {return "translate(" + xScale(d.quintile) + ",0)"; })
-          .selectAll("rect")
-          .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
-          .enter().append("rect")
-          .attr("x", function(d) { return x1(d.key); })
-          .attr("y", function(d) { return yScale(d.value); })
-          .attr("width", x1.bandwidth())
-          .attr("height", function(d) { return barHeight - yScale(d.value); })
-          .attr("fill", function(d) { return chartColor(d.key); })
-          .on("mouseover", handleMouseOver)
-          .on("mouseout", handleMouseOut);
-
-          $("#submit-1").click(function(){
-          })
-   
-    const expRowChart = new dc.RowChart("#row-chart");
-    const expPieChart = new dc.PieChart("#pie-chart");
-    let rowMargin = {top: 8, right: 30, bottom: 30, left: 50};
-    let rowWidth = $("#bar-chart").width()-rowMargin.right-rowMargin.left;
-    let rowHeight = 600-rowMargin.top-rowMargin.bottom;           
+                  
           
-    let ndx = crossfilter(expData), 
-        catDim = ndx.dimension(function(d){return d.category;}),
-        sourceDim = ndx.dimension(function(d){return d.source;}),
-        spendPerCat = catDim.group().reduceSum(function(d){return +(d.spending.toFixed(2))}),
-        spendPerSource = sourceDim.group().reduceSum(function(d){return parseFloat(+d.spending.toFixed(2))});
+  
 
-    let rowColor = d3.scaleOrdinal(d3.quantize(d3.interpolateHcl("#488455", "#68ab77"), 9));
-
-    expRowChart
-      .width(rowWidth).height(rowHeight)
-      .dimension(catDim, "catDim")
-      .group(spendPerCat, "Spend")
-      .ordering(function(d){
-       //console.log(d);
-       return -d.value;})
-      .colors(rowColor)
-      .elasticX(true)
-      .title(function(d){
-          d.value = parseFloat(d.value.toFixed(2));
-          return d.key+ ": "+ d.value;
-        });
-
-      expPieChart
-     .width(300).height(300)
-     .dimension(sourceDim)
-     .group(spendPerSource)
-     .radius(250)
-     .innerRadius(50)
-     .colors(d3.scaleOrdinal(d3.quantize(d3.interpolateHcl("#488455", "#68ab77"), 2)))
-     .title(function(d){
-        d.value = parseFloat(d.value.toFixed(2));
-        return d.key+ ": "+ d.value;
-      })
-      .legend(new dc.HtmlLegend().container('#pie-legend').horizontal(false).highlightSelected(true))
-     .turnOnControls(true);
-
-     dc.renderAll();
-     dc.redrawAll();
+    
+     
    
 }); //end jQuery document ready
--- js/utils.js
+++ js/utils.js
@@ -1,4 +1,13 @@
-let testObj;
+let quintile= 'Second'; 
+let foodAndBeverage = 0;
+let housingUtils = 0;
+let health = 0;
+let transport = 0;
+let communication = 0;
+let recreation = 0;
+let education = 0;
+let foodServices = 0;
+let misc = 0;
 
 const toolTip= d3.select("#bar-chart").append("div")	
                    .attr("id", "tooltip")				
@@ -32,7 +41,7 @@ function storageAvailable(type) {
 function getFromStorage(storage,keyname){
       if (storage.getItem(keyname)) {
         console.log('Getting from Storage instead');
-        testObj=storage.getItem(keyname);   
+        storage.getItem(keyname);   
         return jQuery.parseJSON(storage.getItem(keyname));  
   }
 }
@@ -103,7 +112,59 @@ let cells = trow.selectAll('td')
                 .text(function(d){
                     return d;
                 })   
-}
+}//end push to table
+
+  function populateVar(data){
+    quintile = $("select[name=userQuintile").val();
+    foodAndBeverage = $("input[name=foodBeverage]").val();
+    housingUtils = $("input[name=housingUtils]").val();
+    health = $("input[name=health]").val()
+    transport = $("input[name=transport]").val();
+    communication = $("input[name=communication").val();
+    recreation = $("input[name=recreation").val();
+    education = $("input[name=education").val();
+    foodServices = $("input[name=foodServices").val();
+    misc = $("input[name=misc").val();
+
+    $.each(data,function(i,d){
+      if (d.source=='user'){
+          d.quintile=quintile;
+          switch(d.category) {
+              case "Food and Non-Alcoholic Beverages":
+                  d.spending = foodAndBeverage;
+                  break;
+              case "Housing and Utilities":
+                  d.spending = housingUtils;
+                  break;
+              case "Health":
+                  d.spending = health;
+                  break;
+              case "Transport":
+                  d.spending = transport;
+                  break;
+              case "Communication":
+                  d.spending = communication;
+                  break;
+              case "Recreation":
+                  d.spending = recreation;
+                  break;
+              case "Educational Services":
+                  d.spending = education;
+                  break;
+              case "Food Serving Services":
+                  d.spending = foodServices;
+                  break;
+              case "Misc. Goods and Services":
+                  d.spending = misc;
+                  break;
+              default:
+                  d.spending = 0;
+          } //end switch
+          }
+      });
+
+      return data;
+  }//end populate variables
 
   function handleMouseOver(d, i) {  // Add interactivity
 
@@ -117,11 +178,11 @@ let cells = trow.selectAll('td')
 
 toolTip.style("opacity",1)
       .style("left",  x + "px")		
-      .style("top",  y + "px")        
+      .style("top",  (y-328) + "px")        
       .html(d.key + "<br>" + '$'+d.value)
       .attr("data-type",d.key);
 
-  }
+  }//end handleMouseOver
 
   function handleMouseOut(d, i) {  
     d3.select(this)
@@ -130,4 +191,129 @@ toolTip.style("opacity",1)
       return d.color;})
       .duration(300);
       toolTip.style("opacity",0);
+  }//end handleMouseOut
+
+  function drawBar(data){
+    let barMargin = {top: 8, right: 30, bottom: 30, left: 50};
+    let barWidth = $("#bar-chart").width()-barMargin.right-barMargin.left;
+    let barHeight = 250-barMargin.top-barMargin.bottom;         /*$("#bar-chart").height();*/
+ 
+    // List of subgroups = header of the table 
+     let subgroups=Object.keys(data[0]).splice(2,3);
+ 
+     //List of groups
+     let groups = d3.map(data, function(d){
+         return(d.quintile)}).keys()
+ 
+     
+     let listOfValues = data.map(d=>d['average total income']).concat(data.map(a=>a['average total expenditure']));
+ 
+     // append the svg object to the body of the page
+     let barSvg = d3.select("#bar-chart")
+                     .append("svg")
+                     .attr("width", barWidth+barMargin.right+barMargin.left)
+                     .attr("height", barHeight+barMargin.top+barMargin.bottom)
+                     .append("g")
+                     .attr("transform","translate(" + barMargin.left + "," + barMargin.top+ ")");
+     
+     let xScale = d3.scaleBand()
+             .domain(groups)
+             .range([0,barWidth])
+             .padding([0.25]);
+     barSvg.append("g")
+             .attr("transform","translate(0," + barHeight + ")")
+             .style("font-size", "0.75rem")
+             .call(d3.axisBottom(xScale).tickSize(0));
+     barSvg.append('text')        
+           .text('Income Quintile')
+           .attr("x", 250)
+           .attr("y", 240);
+         
+     let yScale = d3.scaleLinear()
+             .domain([0, d3.max(listOfValues)])
+             .range([ barHeight, 0 ]).nice();
+ 
+     let yAxis = d3.axisLeft(yScale)
+                   .tickFormat(function(d,i){
+                       return Math.round(d/1000).toFixed(0);
+                   });
+ 
+         barSvg.append("g")
+             .call(yAxis);
+             // text label for the y axis
+         barSvg.append("text")
+               .text("$('000)")
+               .attr("x", -150)
+               .attr("y", -30)
+               .attr("transform", "rotate(270)");
+       
+     //subgroup axis
+     let x1 = d3.scaleBand()
+                 .domain(subgroups)
+                 .range([0,xScale.bandwidth()])
+                 .padding([0.05]);
+ 
+     let chartColor = d3.scaleOrdinal()
+                         .domain(subgroups)
+                         .range(['#68ab77','#d8485c']);
+ 
+     
+     //Plot the bars
+     barSvg.append("g")
+           .selectAll("g")
+     // Enter in data = loop group per group
+           .data(data)
+           .enter()
+           .append("g")
+           .attr("transform", function(d) {return "translate(" + xScale(d.quintile) + ",0)"; })
+           .selectAll("rect")
+           .data(function(d) { return subgroups.map(function(key) { return {key: key, value: d[key]}; }); })
+           .enter().append("rect")
+           .attr("x", function(d) { return x1(d.key); })
+           .attr("y", function(d) { return yScale(d.value); })
+           .attr("width", x1.bandwidth())
+           .attr("height", function(d) { return barHeight - yScale(d.value); })
+           .attr("fill", function(d) { return chartColor(d.key); })
+           .on("mouseover", handleMouseOver)
+           .on("mouseout", handleMouseOut);
+  }//end draw bar chart
+
+  function renderPlots(RowChart, PieChart, ndx, catDim, sourceDim, spendPerCat, spendPerSource){
+
+    let rowMargin = {top: 8, right: 30, bottom: 30, left: 50};
+    let rowWidth = $("#row-chart").width()-rowMargin.right-rowMargin.left;
+    let rowHeight = 300-rowMargin.top-rowMargin.bottom; 
+    let rowColor = d3.scaleOrdinal(d3.quantize(d3.interpolateHcl("#488455", "#68ab77"), 9));
+
+    RowChart
+    .width(rowWidth).height(rowHeight)
+    .dimension(catDim, "catDim")
+    .group(spendPerCat, "Spend")
+    .ordering(function(d){
+     //console.log(d);
+     return -d.value;})
+    .colors(rowColor)
+    .elasticX(true)
+    .title(function(d){
+        d.value = parseFloat(d.value.toFixed(2));
+        return d.key+ ": "+ d.value;
+      });
+
+    PieChart
+   .width(300).height(150)
+   .dimension(sourceDim)
+   .group(spendPerSource)
+   .radius(250)
+   .innerRadius(50)
+   .colors(d3.scaleOrdinal(d3.quantize(d3.interpolateHcl("#68ab77", "#d8485c"), 2)))
+   .title(function(d){
+      d.value = parseFloat(d.value.toFixed(2));
+      return d.key+ ": "+ d.value;
+    })
+    .legend(new dc.HtmlLegend().container('#pie-legend').horizontal(false).highlightSelected(true))
+   .turnOnControls(true);
+
+   dc.renderAll();
+  
+
   }
